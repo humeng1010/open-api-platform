29c6f3531a3ca8e473c7ad35be71fe0b8a5ff619
humeng <humeng_mail@163.com>
2023-07-28 23:14:06 +0800

    update: 新增用户表字段

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -25,6 +25,14 @@ create table if not exists user
     index idx_unionId (unionId)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
+# 新增调用接口的密钥
+alter table user
+    add accessKey varchar(512) not null comment 'accessKey' after userRole;
+
+alter table user
+    add secretKey varchar(512) not null comment 'secretKey' after accessKey;
+
+
 -- 帖子表
 create table if not exists post
 (
@@ -69,16 +77,16 @@ create table if not exists post_favour
 -- 接口信息
 create table if not exists interface_info
 (
-    `id` bigint not null auto_increment comment '主键' primary key,
-    `name` varchar(256) not null comment '名称',
-    `description` varchar(256) null comment '描述',
-    `url` varchar(512) not null comment '接口地址',
-    `requestHeader` text null comment '请求头',
-    `responseHeader` text null comment '响应头',
-    `status` int default 0 not null comment '接口状态（0-关闭，1-开启）',
-    `method` varchar(256) not null comment '请求类型',
-    `userId` bigint not null comment '创建人',
-    `createTime` datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    `updateTime` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    `isDelete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
+    `id`             bigint                             not null auto_increment comment '主键' primary key,
+    `name`           varchar(256)                       not null comment '名称',
+    `description`    varchar(256)                       null comment '描述',
+    `url`            varchar(512)                       not null comment '接口地址',
+    `requestHeader`  text                               null comment '请求头',
+    `responseHeader` text                               null comment '响应头',
+    `status`         int      default 0                 not null comment '接口状态（0-关闭，1-开启）',
+    `method`         varchar(256)                       not null comment '请求类型',
+    `userId`         bigint                             not null comment '创建人',
+    `createTime`     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    `updateTime`     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    `isDelete`       tinyint  default 0                 not null comment '是否删除(0-未删, 1-已删)'
 ) comment '接口信息';

268d10e84aa140c737a0b118982669a4e64f38ea
humeng <humeng_mail@163.com>
2023-08-24 00:00:51 +0800

    update: redisson的Bean的创建

--- panda-api-server/src/main/java/com/panda/springbootinit/config/RedissonConfig.java
+++ panda-api-server/src/main/java/com/panda/springbootinit/config/RedissonConfig.java
@@ -1,24 +1,37 @@
 package com.panda.springbootinit.config;
 
+import lombok.Data;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Redisson 配置
+ */
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
 public class RedissonConfig {
-    @Value("${spring.redis.host}")
+
     private String host;
 
-    @Value("${spring.redis.port}")
     private String port;
+
+    // private String password;
 
     @Bean
     public RedissonClient redissonClient() {
+        // 1.创建配置
         Config config = new Config();
-        config.useSingleServer().setAddress(host).setDatabase(0);
+        String redisAddress = String.format("redis://%s:%s", host, port);
+        config.useSingleServer()
+                .setAddress(redisAddress)
+                // .setPassword(password)
+                .setDatabase(0);
+        // 2.创建redis实例
         return Redisson.create(config);
     }
 }

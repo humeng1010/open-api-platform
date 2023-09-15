7692c546542ceba69a753df82f6884859d0a3a4d
humeng <humeng_mail@163.com>
2023-09-15 09:40:17 +0800

    update: 启动运行

--- panda-api-server/src/main/java/com/panda/springbootinit/config/RedissonConfig.java
+++ panda-api-server/src/main/java/com/panda/springbootinit/config/RedissonConfig.java
@@ -2,10 +2,8 @@ package com.panda.springbootinit.config;
 
 import lombok.Data;
 import org.redisson.Redisson;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,27 +33,5 @@ public class RedissonConfig {
                 .setDatabase(0);
         // 2.创建redis实例
         return Redisson.create(config);
-    }
-
-    @Autowired
-    public void testLock(RedissonClient redissonClient) throws InterruptedException {
-        RLock lock = redissonClient.getLock("lock");
-        try {
-            boolean isLock = lock.tryLock();
-            if (!isLock) {
-                while (true) {
-                    Thread.sleep(500);
-                    boolean b = lock.tryLock();
-                    if (b) {
-                        break;
-                    }
-                }
-            }
-            //    加锁后的操作
-
-        } finally {
-            lock.unlock();
-        }
-
     }
 }
